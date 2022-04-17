18708ba99d9b4b9e0c39ab19585a840e481ef718
Qwxp <mmdjoker@yahoo.com>
2022-04-17 16:02:27 +0430

    CSharp Functions

---
+++ PersianSwear-CSharp/PersianSwear/Internal/Vars.cs
@@ -0,0 +1,7 @@
+﻿namespace PersianSwear.Internal;
+
+internal static class Vars
+{
+    internal static string Path = $"{Environment.CurrentDirectory}\\data.json";
+    internal static Words Words = new(Path);
+}
---
+++ PersianSwear-CSharp/PersianSwear/Words.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json;
+
+namespace PersianSwear;
+
+public class Words
+{
+    public Words(string dataPath)
+    {
+        var findFile = new FileInfo(dataPath);
+        var readFile = File.ReadAllText(findFile.FullName);
+        WordCollection = JsonSerializer.Deserialize<List<string>>(readFile) ?? throw new ArgumentNullException($"{nameof(dataPath)}. File Not Found!");
+    }
+    public IReadOnlyCollection<string> WordCollection { get; }
+}
