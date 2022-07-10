b86319c4d8155a1808c9d1e372bf26578759bb65
Amir Shokri <amirsh.nll@gmail.com>
2022-07-10 11:39:16 +0430

    1.6.1

--- PersianSwear.py
+++ PersianSwear.py
@@ -1,70 +1,67 @@
-"""
-swearWords Class
-Author : Sorosh Safari @coci
-created date : 7 October, 2021
-updated date : 11 October, 2021
-"""
 import json
 from string import punctuation
+
+
 class PersianSwear(object):
-	def __init__(self):
-		self.data = json.load(open('data.json'))
+    def __init__(self):
+        self.data = json.load(open('data.json'))
 
-	#Rmove punctuation characters from text
-	# return string
-	def ignoreSY(self, text):
-		for i in punctuation:
-				if i in text:
-					text=text.replace(i,'')
-		return text
-	# return string
-	def filter_words(self, text , symbol="*" , ignoreOT=False):
-		if(self.is_empty()):
-			return text
+    # Rmove punctuation characters from text
+    # return string
+    def ignoreSY(self, text):
+        for i in punctuation:
+            if i in text:
+                text = text.replace(i, '')
+        return text
+    # return string
 
-		text = text.split()
-		for i in range(len(text)):
-			if text[i] in self.data['word'] or (ignoreOT and self.ignoreSY(text[i]) in self.data['word']):
-				text[i] = symbol
+    def filter_words(self, text, symbol="*", ignoreOT=False):
+        if(self.is_empty()):
+            return text
 
-		return " ".join(text)
+        text = text.split()
+        for i in range(len(text)):
+            if text[i] in self.data['word'] or (ignoreOT and self.ignoreSY(text[i]) in self.data['word']):
+                text[i] = symbol
 
-	# return boolean
-	def is_empty(self):
-		if(len(self.data['word'])<1):
-			return True;
-		return False;
+        return " ".join(text)
 
-	# return nothing
-	def add_word(self, text):
-		self.data['word'].append(text)
+    # return boolean
+    def is_empty(self):
+        if(len(self.data['word']) < 1):
+            return True
+        return False
 
-	# return nothing
-	def remove_word(self, text):
-		self.data['word'].remove(text)	
+    # return nothing
+    def add_word(self, text):
+        self.data['word'].append(text)
 
-	# return boolean
-	def is_bad(self, text , ignoreOT=False):
-		if ignoreOT:
-			text=self.ignoreSY(text)
-		text=text.replace("\u200c","")
-		return text in self.data['word']
+    # return nothing
+    def remove_word(self, text):
+        self.data['word'].remove(text)
 
-	# return boolean
-	def has_swear(self, text , ignoreOT=False):
-		if ignoreOT:
-			text=self.ignoreSY(text)
-		text=text.replace("\u200c","")
-		if(self.is_empty()):
-			return text
+    # return boolean
+    def is_bad(self, text, ignoreOT=False):
+        if ignoreOT:
+            text = self.ignoreSY(text)
+        text = text.replace("\u200c", "")
+        return text in self.data['word']
 
-		text = text.split()
-		for i in range(len(text)):
-			if text[i] in self.data['word']:
-				return True
+    # return boolean
+    def has_swear(self, text, ignoreOT=False):
+        if ignoreOT:
+            text = self.ignoreSY(text)
+        text = text.replace("\u200c", "")
+        if(self.is_empty()):
+            return text
 
-		return False
+        text = text.split()
+        for i in range(len(text)):
+            if text[i] in self.data['word']:
+                return True
 
-	# return string
-	def tostring(self):
-		return ' - '.join(self.data['word'])
+        return False
+
+    # return string
+    def tostring(self):
+        return ' - '.join(self.data['word'])
