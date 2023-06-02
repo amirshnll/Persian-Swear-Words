9c177ea267f39e6eb626bb9daefd6cde21afd194
Amir Shokri <amirsh.nll@gmail.com>
2023-06-02 14:25:45 +0330

    Update PersianSwear.php

--- PersianSwear.php
+++ PersianSwear.php
@@ -1,79 +1,56 @@
 <?php
 class PersianSwear
 {
-	private $data;
-	private $data_encoded;
 	private $words;
 
 	public function __construct()
 	{
 		if (file_exists("data.json")) {
-			$this->data 		= file_get_contents('data.json');
-			$this->data_encoded = json_decode($this->data, true);
-			$this->words 		= $this->data_encoded['word'];
-		} else
-			die("dataset file not found!");
+			$data = file_get_contents('data.json');
+			$data_encoded = json_decode($data, true);
+			$this->words = array_flip($data_encoded['word']);
+		} else {
+			die("Dataset file not found!");
+		}
 	}
 
-	// return boolean
 	public function is_empty()
 	{
-		if (count($this->words) < 1)
-			return true;
-		return false;
+		return empty($this->words);
 	}
 
-	// return string
 	public function filter_words($text, $symbol = "*")
 	{
-		if ($this->is_empty())
+		if ($this->is_empty()) {
 			return $text;
-
-		$filterCount = sizeof((array) $this->words);
-
-		for ($i = 0; $i < $filterCount; $i++) {
-			$text = preg_replace('[' . $this->words[$i] . ']', str_repeat($symbol, strlen('$0')), $text);
 		}
-		return $text;
+
+		$pattern = '/\b(' . implode('|', array_keys($this->words)) . ')\b/i';
+		return preg_replace($pattern, str_repeat($symbol, strlen('$0')), $text);
 	}
 
-	// return nothing
 	public function add_word($text)
 	{
-		$this->words[count($this->words)] = ltrim(rtrim($text));
+		$this->words[ltrim(rtrim($text))] = true;
 	}
 
-	// return nothing
 	public function remove_word($text)
 	{
-		$text = ltrim(rtrim($text));
-		for ($i = 0; $i <= count($this->words) - 1; $i++)
-			if ($this->words[$i] == $text)
-				$this->words[$i] = null;
+		unset($this->words[ltrim(rtrim($text))]);
 	}
 
-	// return boolean
 	public function is_bad($text)
 	{
-		if (empty(ltrim(rtrim($this->filter_words($text, "")))))
-			return true;
-		return false;
+		return empty($this->filter_words($text, ''));
 	}
 
-	// return boolean
 	public function has_swear($text)
 	{
-		$text_splited = str_split(ltrim(rtrim($text, " ")));
-		foreach ($text_splited as $key => $val) {
-			if ($this->is_bad($val))
-				return true;
-		}
-		return false;
+		return $this->is_bad($text);
 	}
 
-	// return string
 	public function tostring()
 	{
-		return implode(" ", (array) $this->data);
+		return implode(" ", array_keys($this->words));
 	}
 }
